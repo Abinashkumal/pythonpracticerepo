676a70c769aeddda577b25f380ab1df2ffc9acfd
Abinashkumal <abinashkumal45@gmail.com>
2026-05-29 12:05:37 +0545

    add a sum of two number

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from python!")
+# simple additon of two number in python
 
+#define a fucntion to add two number
+def sum(a , b):
+    sum = a + b 
+    return sum
 
-if __name__ == "__main__":
-    main()
+# take a two interger number of users.
+firstNumber= int(input("Enter a number a : "))
+secondNumber= int(input("please enter a number b: "))
+result =sum(firstNumber , secondNumber)  # function call
+print(f"The sum of two number is {result}") #print a result sums of two number
